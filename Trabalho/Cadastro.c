1fc2acfc68ce7611c5f57da5976ac99670086b68
lucasimroth <lucas-imroth@outlook.com.br>
2023-10-24 15:17:41 -0300

    lucas commit 05- organização
    organizei o código para deixar mais padrão e visível as divisões.
    adicionei a chamada de função relacionada aos clientes na parte superior para poder ordenar abaixo

--- Trabalho/Cadastro.c
+++ Trabalho/Cadastro.c
@@ -14,12 +14,14 @@
 
 */
 
+//---------------------------------------------- Structs ------------------------------------------------------
+
 typedef struct{
 	int estado;
 	char *cidade;
 	char *rua;
 	int *numero;
-}local
+}local;
 
 typedef struct{
 	int cpf;
@@ -53,47 +55,26 @@ typedef struct{
 	
 }pedido;
 
-/****************/
-/*  PROTÓTIPOS  */
-/****************/
+
+//----------------------------------------------------------  PROTÓTIPOS  -------------------------------------------------------------------
+
 
 int mainMenu();//retorna stateProgram
 void consultaFilmes();
 void consultaClientes();
 void consultaPedidos();
+void cadastroClientes();
+void listagemClientes();
+void consultaCPF();
+void excluirCliente();
+
+//----------------------------------------------------------- MAIN ----------------------------------------------------------------------
 
 int main(){
 
-/*1 ---- a parte de cadastro quando entra na função pede os dados do usuario
-sendo eles:
-CPF - NOME - Email - Telefone - DATA de Nascimento - Sexo - endereço - status do cadastro
--- cada cliente novo realiza um sort para deixar sempre ordenado os cadastros.(a ser definido o tipo de sort)
-*/
 
+	int stateProgram = 0;
 
-/*
-2 ----listagem de clientes:
--- abre direto todos os clientes
-aqui da para consultar todos os clientes ao mesmo tempo, aparecendo somente cpf, nome, telefone, pedidos vigentes e status do cadastro.
-para uma consulta mais detalhada é necessario pesquisar por cpf
--- pergunta se gostaria de consultar um cpf especifico, se sim é só digitar o cpf diretamente que ja ira aparecer, se digitar 0 vai para o menu principal
-*/
-
-/*
-3---- consulta a partir do CPF:
---aqui pede o cpf ou da a opção 0 que volta para o menu principal
-com o cpf sera mostrado todos os dados do cadastro.
--- a cada chamada tem a opção de editar cadastro, desativar cliente, fazer uma nova busca ou sair
-*/
-
-/*
-4---- excluir cliente
--- aqui pede o cpf, todos os dados do cliente são mostrados na tela
-a exclusão apaga do registro todos os dados do cliente e reajusta a numeração de cada cliente para encaixar no processo, excluir o 02 faz o 03 virar 02 e assim consecutivamente
--- pergunta se tem certeza que deseja excluir, se quer somente desativar ou sair dessa tela, e confirmar com um sim ou não se clicar em excluir.
-*/ 
-	int stateProgram = 0
-	
 	//MÁQUINA DE ESTADOS
 	while(stateProgram > -1){
 		switch(stateProgram){
@@ -114,4 +95,51 @@ a exclusão apaga do registro todos os dados do cliente e reajusta a numeração
 	}
 
     return 1;
+}
+
+
+//1 ---------------------------------------- CADASTRO --------------------------------------------------------------
+
+void cadastroClientes(){
+
+/*a parte de cadastro quando entra na função pede os dados do usuario
+sendo eles:
+CPF - NOME - Email - Telefone - DATA de Nascimento - Sexo - endereço - status do cadastro
+-- cada cliente novo realiza um sort para deixar sempre ordenado os cadastros.(a ser definido o tipo de sort)*/
+
+
+
+}
+
+
+//2 ------------------------------------------ listagem de clientes ------------------------------------------------------
+
+void listagemClientes(){
+/* abre direto todos os clientes
+aqui da para consultar todos os clientes ao mesmo tempo, aparecendo somente cpf, nome, telefone, pedidos vigentes e status do cadastro.
+para uma consulta mais detalhada é necessario pesquisar por cpf
+-- pergunta se gostaria de consultar um cpf especifico, se sim é só digitar o cpf diretamente que ja ira aparecer, se digitar 0 vai para o menu principal
+*/
+
+}
+
+
+//3--------------------------------------- consulta a partir do CPF --------------------------------------------------------
+
+void consultaCPF(){
+/*--aqui pede o cpf ou da a opção 0 que volta para o menu principal
+com o cpf sera mostrado todos os dados do cadastro.
+a cada chamada tem a opção de editar cadastro, desativar cliente, fazer uma nova busca ou sair*/
+
+}
+
+
+//4------------------------------------------ excluir cliente --------------------------------------------------------------
+
+void excluirClientes(){
+	/*-- aqui pede o cpf, todos os dados do cliente são mostrados na tela
+a exclusão apaga do registro todos os dados do cliente e reajusta a numeração de cada cliente para encaixar no processo, excluir o 02 faz o 03 virar 02 e assim consecutivamente
+-- pergunta se tem certeza que deseja excluir, se quer somente desativar ou sair dessa tela, e confirmar com um sim ou não se clicar em excluir.*/
+
+
 }
